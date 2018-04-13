a032c68c400daf5a8d10e99b4601e483610a378e
hyunjinv <sherhy.hjj@gmail.com>
2018-04-14 06:22:46 +0900

    understood the javac -cp stuff

--- PrincetonU/Algorithms Part 1/module_1/assignment1/Percolation.java
+++ PrincetonU/Algorithms Part 1/module_1/assignment1/Percolation.java
@@ -3,14 +3,21 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+	// private WeightedQuickUnionUF tree;
+	private int[] tree;
+	private int rowlen;
+
 	public Percolation(int n) {               // create n-by-n grid, with all sites blocked
-	
+		// tree = new WeightedQuickUnionUF(n*n);
+		rowlen = n;
+		tree = new int[n*n];
+		for (int i = 0; i<n*n; i++) tree[i] = 0;
 	}
 	public    void open(int row, int col) {   // open site (row, col) if it is not open already
-	
+		tree[rowlen*row + col] = 1;
 	}
 	public boolean isOpen(int row, int col) { // is site (row, col) open?
-		return true;
+		return tree[rowlen*row + col]==1;
 	}
 
 	public boolean isFull(int row, int col) { // is site (row, col) full?
@@ -18,6 +25,7 @@ public class Percolation {
 	}
 
 	public     int numberOfOpenSites()   {   // number of open sites
+		// return tree.count();
 		return 1;
 	}
 
@@ -26,6 +34,9 @@ public class Percolation {
 	}
 	
 	public static void main(String[] args){	 // test client (optional)
-		System.out.println("sup");
+		Percolation uf = new Percolation(3);
+		uf.open(2,2);
+		System.out.println(uf.isOpen(2,2));
+		System.out.println(uf.isOpen(1,2));
 	}   
 }
